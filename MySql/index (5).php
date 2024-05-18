4d8ec3a05ba0238e7aea75e450d430df7370bd70
Mohit kumar <159981706+techmohitkumar@users.noreply.github.com>
2024-05-18 22:26:31 +0530

    Add files via upload

---
+++ MySql/index (5).php
@@ -0,0 +1,27 @@
+<?php
+require_once "config.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1>Database App For Student</h1>
+
+    <ul>
+        <li><a href="./">Home</a></li>
+        <li><a href="./insert.php">Insert</a></li>
+        <li><a href="./">View</a></li>
+        <li><a href="./">Update</a></li>
+        <li><a href="./">Delete</a></li>
+    </ul>
+
+</body>
+
+</html>
