829b6546ae70334f1b7ba66c16a803894b7bf053
Mohit kumar <159981706+techmohitkumar@users.noreply.github.com>
2024-05-21 23:08:26 +0530

    Add files via upload

---
+++ data-fach/index (6).php
@@ -0,0 +1,22 @@
+<?php
+require_once "config.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <?php require_once 'head.php';?>
+    <center>
+        <img src="./public/crud.jpg" width="50%" height="350px" alt="banner">
+    </center>
+
+</body>
+
+</html>
