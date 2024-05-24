b008d68ff94d3d234da2f079bba4eaef3b27708e
Mohit kumar <159981706+techmohitkumar@users.noreply.github.com>
2024-05-24 21:47:22 +0530

    Delete data-fach/head.php

--- data-fach/head.php
+++
@@ -1,9 +0,0 @@
-<h1>Database App For Student</h1>
-
-<ul>
-    <li><a href="./">Home</a></li>
-    <li><a href="./insert.php">Insert</a></li>
-    <li><a href="./view.php">View</a></li>
-    <li><a href="./">Update</a></li>
-    <li><a href="./">Delete</a></li>
-</ul>
