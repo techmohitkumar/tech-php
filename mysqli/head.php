3da636efe020fb0befeff7d468febd89ceea80da
Mohit kumar <159981706+techmohitkumar@users.noreply.github.com>
2024-05-28 23:24:21 +0530

    Add files via upload

---
+++ mysqli/head.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Database App For Student</h1>
+
+<ul>
+    <li><a href="./">Home</a></li>
+    <li><a href="./reg.php">Regster</a></li>
+    <li><a href="./view.php">View</a></li>
+    <li><a href="./update.php">Update</a></li>
+    <li><a href="./Delete.php">Delete</a></li>
+</ul>
+    
+</body>
+</html>
+
