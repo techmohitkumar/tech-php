3232911177f788127b3ced26017303fd0da8f21c
Mohit kumar <159981706+techmohitkumar@users.noreply.github.com>
2024-05-29 22:16:26 +0530

    Add files via upload

---
+++ mysqli/config.php
@@ -0,0 +1,6 @@
+   
+   <?php
+   $dbInfo=array("host"=>"localhost","user"=>"root","pass"=>"","dbname"=>"studentapp.com");
+
+$dbCon=mysqli_connect($dbInfo["host"],$dbInfo["user"],$dbInfo["pass"],$dbInfo["dbname"]);
+?>
