ef98c8c28790ed39d522b4ba5d491e5e27713fd3
Mohit kumar <159981706+techmohitkumar@users.noreply.github.com>
2024-06-02 14:32:31 +0530

    Add files via upload

---
+++ session/index (7).php
@@ -0,0 +1,35 @@
+<?php
+require_once 'config.php';
+echo @$_SESSION['sid'];
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Online Shop</h1>
+
+    <ul>
+        <li><a href="./">Home</a></li>
+        <li><a href="./create-pass.php">Create Pass</a></li>
+        <li><a href="./shop-1.php">Shop 1</a></li>
+        <li><a href="./shop-2.php">Shop 2</a></li>
+        <li><a href="./shop-3.php">Shop 3</a></li>
+        <li><a href="./shop-3.php">Shop 4</a></li>
+        <li><a href="./logout.php">logout</a></li>
+    </ul>
+
+
+
+
+
+</body>
+
+</html>
