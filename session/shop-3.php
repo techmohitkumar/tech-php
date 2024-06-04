c2d61daa993688f283be0b07effea6eb44b7f8d8
Mohit kumar <159981706+techmohitkumar@users.noreply.github.com>
2024-06-04 22:14:30 +0530

    Add files via upload

---
+++ session/shop-3.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'config.php';
+if($_SESSION['sid']==""){
+    header("location:index.php");
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop 3</title>
+</head>
+
+<body>
+    <hr>
+    <a href="./">Home</a>
+    <hr>
+    <h1>Shop Three : <?php echo $_SESSION['sid'];?></h1>
+    <he>
+
+</body>
+
+</html>
